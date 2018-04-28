a9fb23ca3e38a3523a58c95650b5733de7853ee0
EasyLOB <easylob@gmail.com>
2018-04-28 20:14:21 -0300

    -

--- Chinook.Mvc/Controllers/Chinook/CustomerController.cs
+++ Chinook.Mvc/Controllers/Chinook/CustomerController.cs
@@ -3,7 +3,6 @@ using Chinook.Data;
 using Chinook.Data.Resources;
 using EasyLOB;
 using EasyLOB.Data;
-using EasyLOB.Library.Syncfusion;
 using EasyLOB.Mvc;
 using Newtonsoft.Json;
 using Syncfusion.JavaScript;
